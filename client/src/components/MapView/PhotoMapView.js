a78f36df9d4b6780521b9fa49b4e50cf7faa099b
Eric Maciel <elm2178@columbia.edu>
2019-01-25 11:57:40 -0500

    Realtime user locations

--- client/src/components/MapView/PhotoMapView.js
+++ client/src/components/MapView/PhotoMapView.js
@@ -1,11 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Platform, View, Text } from "react-native";
-import { Constants, Location, Permissions, ImagePicker, MapView } from "expo";
+import { Constants, Location, Permissions, MapView } from "expo";
 import { Button } from "../Button";
 import { Realtime, withRealtime } from "../../realtime";
 import { colors } from "../../helpers/style-helpers";
-import { PlaceOverlay } from "../PlaceOverlay";
 import { Spinner } from "../Spinner";
 import { styles } from "./map-view-styles";
 import RingMarker from "./RingMarker";
@@ -24,12 +23,10 @@ class PhotoMapView extends React.PureComponent {
     this.state = {
       errorMessage: null,
       mapMarkerLocation: null,
-      selectedPlaceId: null,
       showSpinner: false,
     };
 
     this.onMapPress = this.onMapPress.bind(this);
-    this.onUploadPhotoPress = this.onUploadPhotoPress.bind(this);
     this.getLocationAsync = this.getLocationAsync.bind(this);
   }
 
@@ -56,39 +53,6 @@ class PhotoMapView extends React.PureComponent {
     this.setState({
       mapMarkerLocation: coords,
     });
-  }
-
-  async onUploadPhotoPress() {
-    const { mapMarkerLocation } = this.state;
-
-    // get camera roll permission
-    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-
-    // show error if camera roll permission was denied
-    if (status !== "granted") {
-      this.setState({
-        errorMessage: "Permission to access camera roll was denied",
-      });
-      return;
-    }
-
-    // launch image picker
-    const result = await ImagePicker.launchImageLibraryAsync();
-    if (result.cancelled) return;
-
-    this.setState({ showSpinner: true });
-
-    try {
-      // create a new place using the image picker result
-      const place = await Realtime.createNewPlace(result, mapMarkerLocation);
-      this.setState({
-        selectedPlaceId: place._id,
-        mapMarkerLocation: null,
-        showSpinner: false,
-      });
-    } catch (err) {
-      this.setState({ showSpinner: false });
-    }
   }
 
   async getLocationAsync() {
@@ -121,18 +85,6 @@ class PhotoMapView extends React.PureComponent {
   }
 
   render() {
-    let messages = [];
-    let selectedPlace = null;
-
-    if (this.state.selectedPlaceId) {
-      messages = this.props.messages
-        .filter(m => m.placeId === this.state.selectedPlaceId)
-        .sort((a, b) => a.createdAt - b.createdAt);
-      selectedPlace = this.props.places.find(
-        p => p._id === this.state.selectedPlaceId
-      );
-    }
-
     // filter out any users that do not have locations set yet
     const usersWithCoords = this.props.users.filter(u => !!u.coords);
 
@@ -140,7 +92,6 @@ class PhotoMapView extends React.PureComponent {
       <>
         <MapView
           ref={m => (this.mapView = m)}
-          provider="google"
           style={styles.mapView}
           initialRegion={initialCoords}
           onPress={this.onMapPress}
@@ -155,22 +106,6 @@ class PhotoMapView extends React.PureComponent {
               backgroundColor={colors.opaqueGreen}
             />
           ))}
-          {this.props.places.map(p => (
-            <MapView.Marker
-              key={p._id}
-              coordinate={p.coords}
-              pinColor={colors.black}
-              title=""
-              onPress={e => {
-                e.preventDefault();
-                e.stopPropagation();
-                this.setState({ selectedPlaceId: p._id });
-              }}
-            >
-              {/* used to hide callout on ios */}
-              <MapView.Callout tooltip />
-            </MapView.Marker>
-          ))}
           {this.state.mapMarkerLocation && (
             <RingMarker
               title="Current Location"
@@ -179,24 +114,10 @@ class PhotoMapView extends React.PureComponent {
             />
           )}
         </MapView>
-        <View style={styles.uploadButtonWrapper}>
-          <Button
-            onPress={this.onUploadPhotoPress}
-            title="Upload photo"
-            disabled={!this.state.mapMarkerLocation}
-          />
-        </View>
         {this.state.errorMessage && (
           <View style={styles.errorMessageWrapper}>
             <Text>{this.state.errorMessage}</Text>
           </View>
-        )}
-        {selectedPlace && (
-          <PlaceOverlay
-            place={selectedPlace}
-            onHide={() => this.setState({ selectedPlaceId: null })}
-            messages={messages}
-          />
         )}
         {this.state.showSpinner && <Spinner />}
       </>
@@ -205,14 +126,10 @@ class PhotoMapView extends React.PureComponent {
 }
 
 PhotoMapView.propTypes = {
-  messages: PropTypes.arrayOf(PropTypes.object),
-  places: PropTypes.arrayOf(PropTypes.object),
   users: PropTypes.arrayOf(PropTypes.object),
 };
 
 PhotoMapView.defaultProps = {
-  messages: [],
-  places: [],
   users: [],
 };
 
--- client/src/components/PlaceOverlay/index.js
+++
@@ -1,1 +0,0 @@
-export { default as PlaceOverlay } from "./PlaceOverlay";
