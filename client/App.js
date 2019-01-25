fb3ca979d963f7ddfbc1f68c5778ad4562f5fda4
Eric Maciel <elm2178@columbia.edu>
2019-01-25 14:58:08 -0500

    Remove counter logic

--- client/App.js
+++ client/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { Button } from "./src/components/Button";
 
 export const styles = StyleSheet.create({
@@ -15,34 +15,17 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      count: 0,
-    };
-
     this.handleButtonPress = this.handleButtonPress.bind(this);
   }
 
   handleButtonPress() {
-    // Not a good idea
-    // this.setState({ count: this.state.count + 1 });
-
-    // Better
-    this.setState(prevState => {
-      // use prev state to calculate next state's count
-      const count = prevState.count + 1;
-
-      // return the updated count
-      return {
-        count,
-      };
-    });
+    console.log("button pressed");
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Button title="Click Me" onPress={this.handleButtonPress} />
-        <Text>Button pressed {this.state.count}</Text>
       </View>
     );
   }
