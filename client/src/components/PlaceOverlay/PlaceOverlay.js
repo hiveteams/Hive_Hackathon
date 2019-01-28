213c2336dfd19ea905e1ee8e15d8aaceac6f0d5f
Eric Maciel <elm2178@columbia.edu>
2019-01-28 12:45:21 -0500

    Clean up place overlay

--- client/src/components/PlaceOverlay/PlaceOverlay.js
+++ client/src/components/PlaceOverlay/PlaceOverlay.js
@@ -1,11 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { View, Image, Text, TouchableOpacity, FlatList } from "react-native";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { Button } from "../Button";
+import { View, Image, Text, TouchableOpacity } from "react-native";
 import { TextInput } from "../TextInput";
 import { Realtime } from "../../realtime";
-import { MessageItem } from "../MessageItem";
 import { styles } from "./place-overlay-styles";
 
 class PlaceOverlay extends React.PureComponent {
@@ -14,7 +11,6 @@ class PlaceOverlay extends React.PureComponent {
 
     this.state = {
       placeName: props.place.name,
-      messageText: "",
     };
 
     this.savePlaceName = this.savePlaceName.bind(this);
